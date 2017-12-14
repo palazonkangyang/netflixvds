67bf9a875486d3ceb9f08d33833fcbb400e8484e
oakkamyo <oakkamyo13@gmail.com>
2017-12-14 09:37:26 +0800

    v0.1.32

--- resources/views/instruction/faq.blade.php
+++ resources/views/instruction/faq.blade.php
@@ -30,72 +30,76 @@
         <div class="col-md-12">
           <h5 class="sub-head-line">Video Module</h5>
 
-          <h6 class="heading6">Q: Why video upload will be fail?</h6>
+          <h6 class="heading6">Q: Why do video uploads sometimes fail?</h6>
           <p>
-            A: There is two possibility, one is you close the browser before the uploading is fully loaded. Another is you
-            try to upload the file which is bigger than 512MB. Please inform admin or Palazon
-            <a href="mailto:support@palazon.com" target="_top">(support@palazon.com)</a> to adjust accordingly.
+            A: There is two possibilities: -
+            <ol>
+              <li>You closed the browser before uploading was closed.</li>
+              <li>You tried to upload a file which is bigger than 512MB. Please inform admin or
+                <a href="mailto:support@palazon.com" target="_top">Palazon(support@palazon.com)</a> to adjust accordingly.</li>
+            </ol>
           </p>
 
-          <h6 class="heading6">Q: Are all the fields in the forms needed to be entered?</h6>
+          <h6 class="heading6">Q: Are all fields in the forms required?</h6>
           <p class="paragraph">
-            A: Only the fields with the red asterisks symbol are compulsory to be entered.
+            A: Only the fields with red asterisks are required.
           </p>
 
           <h5 class="sub-head-line">Store Module</h5>
 
-          <h6 class="heading6">Q: How to upload stores in CSV format?</h6>
+          <h6 class="heading6">Q: How can I upload new stores to the system?</h6>
           <p>
-            A: You need to get the template from <a href="{{ URL::to('download/store_template.xlsx') }}">here</a>, remain the headers, and insert the stores
-              details' fields. store_name, partner_name and country_name need to be same in the system.
+            A: fromYou can only upoad new stores in CSV format <a href="{{ URL::to('download/store_template.xlsx') }}">template here</a> -
+            Leave the headers unchanged and insert the stores detail' fields:- store_name, partner_name and country_name need to be
+            reflected the same in the system.
           </p>
 
-          <h6 class="heading6">Q: Why Country’s dropdown list is empty?</h6>
+          <h6 class="heading6">Q: Why is the Country dropdown list empty?</h6>
           <p>
-            A: You need to select Partner first, then respective country for that partner will be shown in dropdown list.
+            A: You need to select a Partner first, then respective countries for that partner will be shown in the dropdown list.
           </p>
 
-          <h6 class="heading6">Q: Are all the fields in the forms needed to be entered?</h6>
+          <h6 class="heading6">Q: Are all fields in the forms required?</h6>
           <p class="paragraph">
-            A: Only the fields with the red asterisks symbol are compulsory to be entered.
+            A: Only the fields with red asterisks are required.
           </p>
 
           <h5 class="sub-head-line">Country Module</h5>
 
-          <h6 class="heading6">Q: Why some countries cannot be removed?</h6>
+          <h6 class="heading6">Q: Why is that some countries cannot be removed?</h6>
           <p>
-            A: If there are stores under the country, you cannot remove that particular country unless stores
+            A: If there are stores under a country, you cannot remove that particular country unless stores
             under that particular country have been deleted.
           </p>
 
-          <h6 class="heading6">Q: Are all the fields in the forms needed to be entered?</h6>
+          <h6 class="heading6">Q: Are all fields in the forms required?</h6>
           <p class="paragraph">
-            A: Only the fields with the red asterisks symbol are compulsory to be entered.
+            A: Only the fields with red asterisks are required.
           </p>
 
           <h5 class="sub-head-line">User Module</h5>
 
-          <h6 class="heading6">Q: Why there are no partner, country, and store fields in user module?</h6>
+          <h6 class="heading6">Q: Why are there no partner, country, and store fields in user module?</h6>
           <p>
-            A: You need to select on Role field, then respective partner, country, and store fields will be appeared according to the role.
+            A: You need to select the Role field, then respective partner, country, and before store field appear according to the role.
           </p>
 
-          <h6 class="heading6">Q: Are all the fields in the forms needed to be entered?</h6>
+          <h6 class="heading6">Q: Are all fields in the forms required?</h6>
           <p class="paragraph">
-            A: Only the fields with the red asterisks symbol are compulsory to be entered.
+            A:Only the fields with red asterisks are required.
           </p>
 
           <h5 class="sub-head-line">Partner Module</h5>
 
-          <h6 class="heading6">Q: Why some partners cannot be removed?</h6>
+          <h6 class="heading6">Q: Why do I have difficulty removing some partners?</h6>
           <p>
             A: If there are countries under the partner, you cannot remove that particular partner unless countries
             under that particular partner have been deleted.
           </p>
 
-          <h6 class="heading6">Q: Are all the fields in the forms needed to be entered?</h6>
+          <h6 class="heading6">Q: Are all fields in the forms required?</h6>
           <p>
-            A: Only the fields with the red asterisks symbol are compulsory to be entered.
+            A: Only the fields with red asterisks are required.
           </p>
         </div><!-- end col-md-12 -->
         @endif
